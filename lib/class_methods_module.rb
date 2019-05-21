65a59e8ac2fdbdb5328d6b01c23d9c0f2132bf38
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-21 20:54:15 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,14 @@
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+
+  class Dancer
+  extend MetaDancing
+  end
+
+  class Dancer
+  extend MetaDancing
+  end
+end
